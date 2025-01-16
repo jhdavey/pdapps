0ac78da130cb6ebf0b630e2a14255204a61230be
Harley Davey <jhdavey5182@gmail.com>
2025-01-16 12:12:44 -0500

    added new note view and textfield to view

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -1,32 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:pd/services/crud/crud_exceptions.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' show join;
-
-const dbName = 'notes.db';
-const notesTable = 'note';
-const userTable = 'user';
-const idColumn = 'id';
-const emailColumn = 'email';
-const userIdColumn = 'user_id';
-const textColumn = 'text';
-const isSyncedWithCloudColumn = 'is_synced_with_cloud';
-const createUserTable = '''CREATE TABLE IF NOT EXISTS "user" (
-  "id"	INTEGER NOT NULL,
-  "email"	TEXT NOT NULL UNIQUE,
-  PRIMARY KEY("id" AUTOINCREMENT)
-)''';
-const createNoteTable = '''CREATE TABLE IF NOT EXISTS "note" (
-  "id"	INTEGER NOT NULL,
-  "user_id"	INTEGER NOT NULL,
-  "text"	TEXT,
-  "is_synced_with_cloud"	INTEGER NOT NULL DEFAULT 0,
-  PRIMARY KEY("id","user_id"),
-  FOREIGN KEY("user_id") REFERENCES "user"("id")
-);''';
 
 class NotesService {
   Database? _db;
@@ -322,3 +299,26 @@ class DatabaseNote {
   @override
   int get hashCode => id.hashCode;
 }
+
+const dbName = 'notes.db';
+const notesTable = 'note';
+const userTable = 'user';
+const idColumn = 'id';
+const emailColumn = 'email';
+const userIdColumn = 'user_id';
+const textColumn = 'text';
+const isSyncedWithCloudColumn = 'is_synced_with_cloud';
+
+const createUserTable = '''CREATE TABLE IF NOT EXISTS "user" (
+  "id"	INTEGER NOT NULL,
+  "email"	TEXT NOT NULL UNIQUE,
+  PRIMARY KEY("id" AUTOINCREMENT)
+);''';
+const createNoteTable = '''CREATE TABLE IF NOT EXISTS "note" (
+  "id"	INTEGER NOT NULL,
+  "user_id"	INTEGER NOT NULL,
+  "text"	TEXT,
+  "is_synced_with_cloud"	INTEGER NOT NULL DEFAULT 0,
+  FOREIGN KEY("user_id") REFERENCES "user"("id"),
+  PRIMARY KEY("id" AUTOINCREMENT)
+);''';
