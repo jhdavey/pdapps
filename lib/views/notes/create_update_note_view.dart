389f9f69355bc1246c954dd9f1e59fedbc2f837f
Harley Davey <jhdavey5182@gmail.com>
2025-01-16 21:54:22 -0500

    added share function with share_plus plugin

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:pd/services/auth/auth_service.dart';
 import 'package:pd/services/cloud/cloud_note.dart';
 import 'package:pd/services/cloud/firebase_cloud_storage.dart';
+import 'package:pd/utilities/dialogs/cannot_show_emtpy_note_dialog.dart';
 import 'package:pd/utilities/generics/get_arguments.dart';
+import 'package:share_plus/share_plus.dart';
 
 class CreateUpdateNoteView extends StatefulWidget {
   const CreateUpdateNoteView({super.key});
@@ -93,6 +95,19 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('New Note'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final text = _textController.text;
+              if (_note == null || text.isEmpty) {
+                await showCannotShareEmptyNoteDialog(context);
+              } else {
+                Share.share(text);
+              }
+            },
+            icon: Icon(Icons.share),
+          ),
+        ],
       ),
       body: FutureBuilder(
         future: createOrGetExistingNote(context),
